5e2a4e62401d41f08f968388259de82bffd9f556
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2022-11-22 12:10:16 +1300

    Delete move_type and create proper move class in engine

---
+++ src/engine/move.cpp
@@ -0,0 +1,74 @@
+#include "engine/move.h"
+
+#include <cassert>
+#include <algorithm> // for std::max
+
+
+// move format: (from right to left)
+// 1 bits: move exists flag, allows us to return no move found
+// 5 bits: starting position (range 0-31)
+// 4 bits: number of jumps, zero if normal move (range 0-9)
+// 2 bits: direction of first part of move (range 0-3)
+// 2 bits: direction of second part of move [optional - only applies if double jump]
+// 2 bits ... [for triple jumps and above]
+// maximum move length = 1 + 5 + 4 + (2 * 9) = 28 (fits within uin32_t)
+
+
+Move::Move(int starting_position, bool is_jump, int direction) {
+	assert(starting_position >= 0 && starting_position < 32);
+	assert(direction >= 0 && direction < 4);
+
+	m_data = 1 << EXISTS_SHIFT
+		| starting_position << START_POS_SHIFT
+		| static_cast<int>(is_jump) << NUM_JUMPS_SHIFT
+		| direction << DIRECTION_LIST_SHIFT;
+}
+
+
+void Move::addJumpDirection(int direction) {
+	assert(exists());
+	assert(direction >= 0 && direction < 4);
+	assert(getNumberOfJumps() > 0 && getNumberOfJumps() < MAX_JUMPS);
+
+	m_data |= direction << (DIRECTION_LIST_SHIFT + DIRECTION_LIST_ITEM_WIDTH * getNumberOfJumps());
+	m_data += 1 << NUM_JUMPS_SHIFT;
+}
+
+
+bool Move::exists() const {
+	return getField(EXISTS_SHIFT, EXISTS_WIDTH);
+}
+
+
+int Move::getStartingPosition() const {
+	assert(exists());
+
+	return getField(START_POS_SHIFT, START_POS_WIDTH);
+}
+
+
+int Move::getNumberOfJumps() const {
+	assert(exists());
+
+	return getField(NUM_JUMPS_SHIFT, NUM_JUMPS_WIDTH);
+}
+
+
+int Move::getDirection(int index) const {
+	assert(exists());
+	assert(index >= 0 && index < std::max(1, getNumberOfJumps()));
+
+	return getField(DIRECTION_LIST_SHIFT + DIRECTION_LIST_ITEM_WIDTH * index, DIRECTION_LIST_ITEM_WIDTH);
+}
+
+
+bool Move::isJump() const {
+	assert(exists());
+
+	return getNumberOfJumps() > 0;
+}
+
+
+int Move::getField(int shift, int width) const {
+	return (m_data >> shift) & ((1 << width) - 1);
+}
---
+++ src/engine/move.h
@@ -0,0 +1,39 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+
+#include <cstdint>
+
+
+class Move {
+public:
+	Move(int starting_position, bool is_jump, int direction);
+	Move() = default; // creates a blank move (doesn't exist)
+
+	void addJumpDirection(int direction);
+
+	bool exists() const;
+	int getStartingPosition() const;
+	int getNumberOfJumps() const;
+	int getDirection(int index) const;
+	bool isJump() const;
+
+	static constexpr int MAX_JUMPS = 9;
+
+private:
+	uint32_t m_data = 0;
+
+	int getField(int shift, int width) const;
+
+	static constexpr int EXISTS_SHIFT = 0;
+	static constexpr int EXISTS_WIDTH = 1;
+	static constexpr int START_POS_SHIFT = 1;
+	static constexpr int START_POS_WIDTH = 5;
+	static constexpr int NUM_JUMPS_SHIFT = 6;
+	static constexpr int NUM_JUMPS_WIDTH = 4;
+	static constexpr int DIRECTION_LIST_SHIFT = 10;
+	static constexpr int DIRECTION_LIST_ITEM_WIDTH = 2;
+};
+
+
+#endif // MOVE_H
