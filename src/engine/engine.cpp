76def509e5bcd3073250c230ad452692faace130
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2022-11-21 17:14:34 +1300

    Move bitboard conversion function into engine.cpp

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -4,8 +4,38 @@
 #include "game/board.h"
 #include "game/move.h"
 #include "game/turn.h"
+#include "game/piece.h"
+#include "game/position.h"
 #include "engine/bitboard.h"
-#include "engine/convert.h"
+
+
+static Bitboard convertBoardToBitboard(const Board &board) {
+	Bitboard bitboard;
+
+	bitboard.black_pieces = 0;
+	bitboard.white_pieces = 0;
+	bitboard.king_pieces = 0;
+
+	for (int position = 0; position < 32; position++) {
+		Piece piece = board.pieceAt(position);
+
+		if (piece.exists()) {
+			u32 mask = 1u << position;
+
+			if (piece.belongsTo(Turn::BLACK)) {
+				bitboard.black_pieces |= mask;
+			} else {
+				bitboard.white_pieces |= mask;
+			}
+
+			if (piece.isCrowned()) {
+				bitboard.king_pieces |= mask;
+			}
+		}
+	}
+
+	return bitboard;
+}
 
 
 Move Engine::findBestMove(const Game &game) {
